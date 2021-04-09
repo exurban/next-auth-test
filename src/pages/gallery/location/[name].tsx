702da4b6c2036cc165124335339a405ed2161b7e
Brad Gibbs <bradgibbs@mac.com>
2021-04-09 10:10:39 -0500

    add locations gallery

---
+++ src/pages/gallery/location/[name].tsx
@@ -0,0 +1,77 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
+import { initializeApollo, addApolloState } from "../../../lib/apolloClient";
+import { AllPhotosAtLocationDocument, AllPhotosAtLocationInput } from "../../../graphql-operations";
+import Loader from "../../../components/Loader";
+import ErrorMessage from "../../../components/ErrorMessage";
+import Gallery from "../../../components/Gallery";
+
+const TagGallery: React.FC = () => {
+  const router = useRouter();
+
+  // * get tag from router
+  const { name } = router.query;
+  let searchString;
+  if (name) {
+    searchString = decodeURIComponent(name as string);
+  }
+
+  const input = { name: searchString } as AllPhotosAtLocationInput;
+
+  const { loading, error, data } = useQuery(AllPhotosAtLocationDocument, {
+    variables: { input: input }
+  });
+
+  if (error) return <ErrorMessage message={`Error loading photos at location: ${name}.`} />;
+
+  if (loading) return <Loader />;
+
+  if (!data) return null;
+
+  const { photos } = data.allPhotosAtLocation;
+
+  return (
+    <>
+      <Gallery photos={photos} />
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const locations = [
+    "Grand Teton National Park",
+    "Great Lakes",
+    "Great Smoky Mountains National Park",
+    "American Southwest",
+    "Unknown",
+    "Yellowstone National Park"
+  ];
+  const paths = locations.map(l => ({ params: { name: l } }));
+  return {
+    paths: paths,
+    fallback: false
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (!params || typeof params.name !== "string") {
+    return;
+  }
+  const input = { name: params.name } as AllPhotosAtLocationInput;
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: AllPhotosAtLocationDocument,
+    variables: { input: input }
+  });
+
+  return addApolloState(apolloClient, {
+    props: {
+      initialApolloState: apolloClient.cache.extract()
+    },
+    revalidate: 1
+  });
+};
+
+export default TagGallery;
